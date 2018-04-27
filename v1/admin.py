388cd7a5218188e4638aacea4a9d3d1473b149a6
Penina Wanjiru <penina.wanjiru@andela.com>
2018-04-27 17:59:04 +0300

    Register models

--- v1/admin.py
+++ v1/admin.py
@@ -2,7 +2,12 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import School, Student, Teacher, Register, Level
 
 # Register your models here.
-from .models import School, Student, Teacher, Register
+
 admin.site.register(School)
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Register)
+admin.site.register(Level)
