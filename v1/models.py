be37f095385bf1dcdbaf35aee9ae6aff18f8c358
Penina Wanjiru <penina.wanjiru@andela.com>
2018-04-27 17:59:38 +0300

    Refactor models

--- v1/models.py
+++ v1/models.py
@@ -7,8 +7,12 @@ class School(models.Model):
         ('boarding', 'BOARDING'),
     )
     name = models.CharField(max_length=255, blank=False, null=False)
-    school_type = models.CharField(max_length=20, choices=school_types, null=False)
+    school_type = models.CharField(max_length=20,
+                                   choices=school_types, null=False)
     email = models.EmailField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Level(models.Model):
@@ -22,26 +26,45 @@ class Level(models.Model):
         ('A', 'a'),
         ('B', 'b'),
         ('C', 'c'),
-        ('D', 'd'),
     )
     stream = models.CharField(choices=stream_choices, max_length=255)
     form = models.IntegerField(choices=form_choices)
 
+    @property
+    def name(self):
+        return "%s%s" % (self.form, self.stream)
+
+    def __str__(self):
+        return self.name
+
 
 class Teacher(models.Model):
     name = models.CharField(max_length=200)
-    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE, related_name="level")
     email = models.EmailField(max_length=255, blank=True)
 
-
-class Register(models.Model):
-    school = models.ForeignKey(School, on_delete=models.CASCADE)
-    teacher = models.ForeignKey(Teacher)
-    date = models.DateField()
-    is_present = models.BooleanField()
+    def __str__(self):
+        return self.name
 
 
 class Student(models.Model):
     name = models.CharField(max_length=255, blank=False, null=False)
     level = models.ForeignKey(Level, on_delete=models.CASCADE)
-    register = models.ForeignKey(Register, related_name="students")
+
+    def __str__(self):
+        return self.name
+
+
+class Register(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE, 
+                                related_name="school")
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE,
+                                related_name="teacher")
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, 
+                                related_name="student")
+    date = models.DateField()
+    is_present = models.BooleanField()
+
+    def __str__(self):
+        return self.date
+
